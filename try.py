0a7bf769f459f6f9905db5e293aefd7879ecdc30
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-10-19 17:45:30 +0700

    update try.py

--- try.py
+++ try.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from pythainlp.tokenize import dict_word_tokenize,create_custom_dict_trie
 data=create_custom_dict_trie("wordlist.txt")
 while True:
  text=input("text : ")
- print(dict_word_tokenize(text,custom_dict_trie=data, engine="newmm")
+ print(dict_word_tokenize(text,custom_dict_trie=data, engine="newmm"))
  print("\r\n")
